c15b38956e9823858f4d27ab7fc6f69d94802436
Moises Santos da Luz <113149089+MoisesLhuz@users.noreply.github.com>
2023-08-29 16:28:34 -0300

    Add files via upload

--- relogio/js/script.js
+++ relogio/js/script.js
@@ -4,6 +4,9 @@ const minuto = document.querySelector("#minutos")
 const segundo = document.querySelector("#segundos")
 const fundo = document.querySelector("#container img")
 const texto = document.querySelector(".description")
+const data = document.querySelector('#data')
+const daySemana = document.querySelector('#diaSemana')
+
 
 // função com setInteral para ocorrer o load de 1 segundo
 const relogio = setInterval(() => {
@@ -14,6 +17,8 @@ const relogio = setInterval(() => {
     let hr = sistema.getHours();
     let min = sistema.getMinutes();
     let seg = sistema.getSeconds();
+    let dataDeHoje = sistema.toLocaleDateString('pt-br')
+    let diaSemana = sistema.getDay()
 
     // condicionais para corrigir bug de 1 digito
     if(hr < 10) {
@@ -25,7 +30,8 @@ const relogio = setInterval(() => {
     if(seg < 10) {
         seg = "0" + seg;
     }
-
+    
+    // estrutura condicional para apresentar as imagens
     if(hr > 6 && hr < 12) {
         fundo.setAttribute('src', 'img/amanhecer.jpg')
         texto.textContent = "Good Morning"
@@ -37,10 +43,40 @@ const relogio = setInterval(() => {
         texto.textContent = "Good Evening"
     }
 
+    
+    switch (diaSemana) {
+        case 0:
+            daySemana.textContent = "Domingo"
+            break;
+        case 1:
+            daySemana.textContent = "Segunda"
+            break;
+        case 2:
+            daySemana.textContent = "Terça"
+            break;
+        case 4:
+            daySemana.textContent = "Quarta"
+            break;
+        case 5:
+            daySemana.textContent = "Quinta"
+            break;
+        case 6:
+            daySemana.textContent = "Sexta"
+            break;
+        case 7:
+            daySemana.textContent = "Sábado"
+            break;    
+        default: "Dia invalido"
+            break;
+    }
+    
+
     // passando resultado para o html
     hora.textContent = hr;
     minuto.textContent = min;
     segundo.textContent = seg;
+    data.textContent = dataDeHoje;
+    
 
 
 },1000);
